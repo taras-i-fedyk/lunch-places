ab91eddae626c4067e60be359e35cc0535c616b2
Taras Fedyk <taras.i.fedyk@gmail.com>
2024-03-11 13:34:23 +0200

    Add a full-screen map to the main screen

--- app/src/main/java/com/tarasfedyk/lunchplaces/ui/MainActivity.kt
+++ app/src/main/java/com/tarasfedyk/lunchplaces/ui/MainActivity.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.maps.android.compose.GoogleMap
 import com.tarasfedyk.lunchplaces.ui.theme.LunchPlacesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -26,21 +24,10 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Content() {
     LunchPlacesTheme {
-        Surface(
-            modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
-        ) {
-            Greeting("Android")
-        }
+        GoogleMap(
+            modifier = Modifier.fillMaxSize()
+        )
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
