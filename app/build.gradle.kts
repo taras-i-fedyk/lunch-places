2ef3ddca8ae1284f84a9fc0cd383234cc3a1c52f
Taras Fedyk <taras.i.fedyk@gmail.com>
2024-03-12 23:53:55 +0200

    Reorder some dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,9 +73,9 @@ dependencies {
     implementation(libs.androidx.hilt.navigation.compose)
 
     // core functionality
-    implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui)
