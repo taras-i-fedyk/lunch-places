bb3ef2d2177de76c31761d4bf71352b7c13782c4
Taras Fedyk <taras.i.fedyk@gmail.com>
2024-03-14 10:38:04 +0200

    Update the scope of an injected dependency

--- app/src/main/java/com/tarasfedyk/lunchplaces/biz/inj/LocationModule.kt
+++ app/src/main/java/com/tarasfedyk/lunchplaces/biz/inj/LocationModule.kt
@@ -8,12 +8,10 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
-import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 object LocationModule {
-    @ViewModelScoped
     @Provides
     fun provideFusedLocationClient(
         @ApplicationContext appContext: Context
