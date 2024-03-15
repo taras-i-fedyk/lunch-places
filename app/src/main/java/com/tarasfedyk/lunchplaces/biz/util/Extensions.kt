28d1d3bfc32fa791536233ba71835a701b17f555
Taras Fedyk <taras.i.fedyk@gmail.com>
2024-03-15 03:21:29 +0200

    Fix how the location permissions are handled

---
+++ app/src/main/java/com/tarasfedyk/lunchplaces/biz/util/Extensions.kt
@@ -0,0 +1,11 @@
+package com.tarasfedyk.lunchplaces.biz.util
+
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.MultiplePermissionsState
+import com.google.accompanist.permissions.isGranted
+
+@OptIn(ExperimentalPermissionsApi::class)
+fun MultiplePermissionsState.isPermissionGranted(permission: String): Boolean =
+    permissions.any {
+        it.permission == permission && it.status.isGranted
+    }
--- app/src/main/java/com/tarasfedyk/lunchplaces/ui/MainActivity.kt
+++ app/src/main/java/com/tarasfedyk/lunchplaces/ui/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.tarasfedyk.lunchplaces.ui
 
-import android.Manifest
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.google.maps.android.compose.GoogleMap
 import com.tarasfedyk.lunchplaces.biz.LocationViewModel
 import com.tarasfedyk.lunchplaces.biz.data.LocationState
+import com.tarasfedyk.lunchplaces.biz.util.isPermissionGranted
 import com.tarasfedyk.lunchplaces.ui.theme.LunchPlacesTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -47,27 +50,40 @@ class MainActivity : ComponentActivity() {
             modifier = Modifier.fillMaxSize()
         )
         LocationPermissionsRequest(
-            onAnyLocationPermissionGranted = { locationViewModel.determineCurrentLocation() }
+            onSomeLocationPermissionGranted = { locationViewModel.determineCurrentLocation() }
         )
     }
 
     @OptIn(ExperimentalPermissionsApi::class)
     @Composable
     private fun LocationPermissionsRequest(
-        onAnyLocationPermissionGranted: () -> Unit
+        onSomeLocationPermissionGranted: () -> Unit
     ) {
         val locationPermissionsState = rememberMultiplePermissionsState(
-            permissions = listOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
+            permissions = listOf(ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION)
         )
-        LaunchedEffect(locationPermissionsState) {
-            if (!locationPermissionsState.allPermissionsGranted) {
-                locationPermissionsState.launchMultiplePermissionRequest()
+        val supportsCoarseLocationOnly by remember {
+            derivedStateOf {
+                locationPermissionsState.isPermissionGranted(ACCESS_COARSE_LOCATION) &&
+                !locationPermissionsState.isPermissionGranted(ACCESS_FINE_LOCATION)
             }
-            if (locationPermissionsState.permissions.any { it.status.isGranted }) {
-                onAnyLocationPermissionGranted()
+        }
+        val supportsFineLocation by remember {
+            derivedStateOf {
+                locationPermissionsState.isPermissionGranted(ACCESS_FINE_LOCATION)
+            }
+        }
+
+        LaunchedEffect(supportsCoarseLocationOnly) {
+            if (supportsCoarseLocationOnly) {
+                onSomeLocationPermissionGranted()
+            }
+        }
+        LaunchedEffect(supportsFineLocation) {
+            if (supportsFineLocation) {
+                onSomeLocationPermissionGranted()
+            } else {
+                locationPermissionsState.launchMultiplePermissionRequest()
             }
         }
     }
